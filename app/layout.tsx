c7d039c4be6332cabc0222128bb59da44559cf1b
Daniel White <daniel_white@hushmail.com>
2023-03-23 11:32:22 -0400

    fix hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang='en' suppressHydrationWarning>
       <Script></Script>
-      <Providers>
-        <body className='bg-gray-100 dark:bg-zinc-900 transition-all duration-500'>
+      <body className='bg-gray-100 dark:bg-zinc-900 transition-all duration-500'>
+        <Providers>
           <Header />
           <div className='max-w-6xl mx-auto'>{children}</div>
-        </body>
-      </Providers>
+        </Providers>
+      </body>
     </html>
   );
 }
